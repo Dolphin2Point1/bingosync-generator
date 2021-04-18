ffe1c535221ef3179029d9c124c22c2612afaf5e
Dolphin2Point1 <finb7729@gmail.com>
2021-04-18 17:57:14 -0500

    yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml yaml

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
 import jsonpickle
 import sys
+import getopt
+import yaml
 
 import pandas
 
 import BingoBoard
 import ChoicePool
 
-dataframe: pandas.DataFrame = pandas.read_excel(io=sys.argv[1])
+opts = getopt.getopt(sys.argv[1:], 'e:y:o:')
 
-choices = dataframe['Bingo Choices']
+excel = False
+file = ""
+
+outputFile = 'output.json'
+
+for o, a in opts:
+    if o == '-e':
+        excel = True
+        file = a
+    elif o == '-y':
+        excel = False
+        file = a
+    elif o == '-o':
+        if not outputFile.endswith(".json"):
+            raise ValueError("Output file must end with .json, currently only json is supported.")
+        outputFile = a
+
+if file == "":
+    raise ValueError("You must specify an input file.")
+
+if excel:
+    dataframe: pandas.DataFrame = pandas.read_excel(io=file)
+    choices = dataframe['Bingo Choices']
+else:
+    yamlInput = yaml.load(file)
+    choices = yamlInput['choices']
 
 choice_pool = ChoicePool.ChoicePool(choices.tolist())
 
